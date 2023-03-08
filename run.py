7e6a55b4827c9b3fa198825bb7e978e77eaf7825
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-03-08 21:40:44 +0000

    Create constant varaibles

--- run.py
+++ run.py
@@ -1,24 +1,21 @@
-from colorama import Fore, Back, Style
+import gspread
+from google.oauth2.service_account import Credentials
 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
-def menu():
-     print(Fore.LIGHTGREEN_EX + """
-    1. Add book
-    2. Edit book
-    3. Remove book
-    4. View all books
-    5. Show book details
-    6. Exit
-    """ + Style.RESET_ALL)
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('home_library')
+books = SHEET.worksheet('books')
+data = books.get_all_values()
+print(data)
 
-menu()
+def view_all_books():
+    print('Updating books...')
 
-def show_menu():
-    while True:
-        menu()
-        user_choice = input(Fore.LIGHTYELLOW_EX
-                            + "Please select a number from 1 to 6 "
-                              "to continue: "
-                            + Style.RESET_ALL)
-        clear_terminal()
-        # validates user input, only values from 1 to 6 are allowed
+view_all_books()
