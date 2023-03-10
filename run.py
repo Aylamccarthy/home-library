c9a6c8f20d76c90c79028e6f6d6300dc8137ec78
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-03-10 09:56:23 +0000

    Define menu and show menu function

--- run.py
+++ run.py
@@ -17,6 +17,34 @@ SHEET = GSPREAD_CLIENT.open('home_library')
 
 books = SHEET.worksheet('books')
 data = books.get_all_values()
+print("Welcome to Home Library App.")
+print("You can manage all your books here.")
+print("Please use menu below to continue.\n")
+
+def menu():
+    """
+    Show all the options 1-6 the user can choose from to manage their books
+    """
+    print(Fore.BLUE + """
+    1. Add book
+    2. Edit book
+    3. Remove book
+    4. View all books
+    5. Show book details
+    6. Exit
+    """+ Style.RESET_ALL)
+
+def show_menu():
+    """
+    will print menu. User is given an option between 1-6.
+    """
+    menu()
+   
+
+
+
+
+
 
 def view_all_books():
     print('Updating books...\n')
