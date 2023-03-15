79705839d57491677599e0f0483f23a87ad8db09
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-03-15 12:39:23 +0000

    Add validate num_range function to check if user input is within desired range

--- run.py
+++ run.py
@@ -129,23 +129,104 @@ def validate_string(user_text, max_length, element):
             element = user_input.title()
             return element
 
+def validate_num_range(user_input, first_val, last_val):
+    """
+    Checks if user input is withing the range of possible options.
+    Any input out of desired range will give user a hint showing
+    a message containing exact range of possible options.
+    :param user_input: this is user input
+    :param first_val: this is first option from the range of options
+    :param last_val:  this is the last option from the range of options
+    :returns True if user's input is valid
+    :returns False if user's input is invalid
+    """
+    try:
+        options = list(range(first_val, last_val + 1))
+        allowed_options = [str(i) for i in options]
 
+        if user_input in allowed_options:
+            return True
+        else:
+            raise ValueError
+    except ValueError:
+        clear_terminal()
+        print(Fore.LIGHTRED_EX +
+              f"\nWrong input, please select option from "
+              f"{first_val} to {last_val} "
+              f"to continue..." + Style.RESET_ALL)
 
+# PrettyTable columns width
+MAX_LEN = {"ID": 2, "Title": 24, "Author": 18, "Category": 12, "Status": 8}
+# PrettyTable table width
+TABLE_MAX_LEN = 79
+# Book details max length for validation of inputs
+ID_MAX_LEN = MAX_LEN["ID"]
+TITLE_MAX_LEN = MAX_LEN["Title"]
+AUTHOR_MAX_LEN = MAX_LEN["Author"]
+CAT_MAX_LEN = MAX_LEN["Category"]
+# description below assigned individually, it is displayed
+# outside the PrettyTable tab.
+DESC_MAX_LEN = 200
+# Separator line
+LINE = Fore.YELLOW + "#"*TABLE_MAX_LEN + Style.RESET_ALL  # 79 characters long
+# Reading status
+READ_YES = "Read"
+READ_NO = "Not read"
 
 def add_book():
-    """Allows user to add new entry to the library.
     """
+    Allows user to add new book to database using user input with following
+    data: author, title, category, read status and description.
+    The ID of the book is generated and added automatically for each new entry.
+    Function looks up the database for first empty row and inserts new entry
+    there. After adding new book the database is re-sorted and all ID values
+    are renumbered to keep ascending order in the database.
+    """
+    
     # initialize variable to store all book details from user input
     book_to_be_added = []
 
     while True:
+        # user inputs title, then it's being validated, max 24 char allowed
+        title = validate_string(Fore.LIGHTCYAN_EX
+                                + "Please enter book's title: "
+                                + Style.RESET_ALL, TITLE_MAX_LEN,
+                                "title")
+        # user inputs author then it's being validated, max 16 char allowed
+        author = validate_string(Fore.LIGHTCYAN_EX
+                                 + "Please enter book's author: "
+                                 + Style.RESET_ALL, AUTHOR_MAX_LEN,
+                                 "author")
+        # user inputs category then it's being validated, max 12 char allowed
+        category = validate_string(Fore.LIGHTCYAN_EX
+                                   + "Please enter book's category: "
+                                   + Style.RESET_ALL, CAT_MAX_LEN,
+                                   "category")
+        # user choose book reading status, allowed input is 1 or 2
+        while True:
+            status = input(Fore.LIGHTCYAN_EX
+                           + "Please select \"1\" if you read that book "
+                             "or \"2\" if you didn't: "
+                           + Style.RESET_ALL)
+            # checks if user input is digit in range 1-2
+            if validate_num_range(status, 1, 2):
+                if status == "1":
+                    read_status = READ_YES
+                    break
+                elif status == "2":
+                    read_status = READ_NO
+                    break
 
+        description = validate_string(
+            Fore.LIGHTCYAN_EX + "Please enter book's description: "
+                              + Style.RESET_ALL, DESC_MAX_LEN,
+                                "description")
 
+        break
 
-
-
-
-
+    # insert all collected inputs into the list
+    book_to_be_added.extend([title, author, category, read_status,
+                             description])
 
 add_book()
 
